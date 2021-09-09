818b7c76e7afa31f786cb46f7a0fd01c5c258b86
Joelle DesChatelets <jdeschat@gmail.com>
2021-09-09 12:18:10 -0400

    added CRUD

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
-    User.findAll()
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -12,9 +14,10 @@ router.get('/', (req, res) => {
         });
 });
 
-// GET /api/users/1
+// GET /api/users/145
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
