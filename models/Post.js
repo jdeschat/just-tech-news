a0280d039858812c78b8d4f880d2d88b8f5b7411
Joelle DesChatelets <jdeschat@gmail.com>
2021-09-10 16:03:42 -0400

    enable usrs to vote on posts

--- models/Post.js
+++ models/Post.js
@@ -1,8 +1,30 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
 // create our Post model
-class Post extends Model { }
+class Post extends Model {
+    static upvote(body, models) {
+        return models.Vote.create({
+            user_id: body.user_id,
+            post_id: body.post_id
+        }).then(() => {
+            return Post.findOne({
+                where: {
+                    id: body.post_id
+                },
+                attributes: [
+                    'id',
+                    'post_url',
+                    'title',
+                    'created_at',
+                    [
+                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
+                        'vote_count'
+                    ]
+                ]
+            });
+        });
+    }
+}
 
 // create fields/columns for Post model
 Post.init(
@@ -24,10 +46,8 @@ Post.init(
                 isURL: true
             }
         },
-        // user_id is conversely defined as the foreign key and will be the matching link
         user_id: {
             type: DataTypes.INTEGER,
-            // using the references property, we establish the relationship between this post and the user by creating a reference to the User model, specifically to the id column that is defined by the key property, which is the primary key
             references: {
                 model: 'user',
                 key: 'id'
