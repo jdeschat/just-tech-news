bc44fbd77d65666db27ccb29739822eeb01a5cc4
Joelle DesChatelets <jdeschat@gmail.com>
2021-09-13 15:58:22 -0400

    finished all files

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
-const commentRoutes = require('./comment-routes');
-
-router.use('/comments', commentRoutes);
-
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
