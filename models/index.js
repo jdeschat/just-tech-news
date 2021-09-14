da38450490aa7608beb36290ca39165c64fbb410
Joelle DesChatelets <jdeschat@gmail.com>
2021-09-14 16:56:34 -0400

    Merge branch 'develop' into feature/homepage-view

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
-const User = require("./User");
-const Post = require("./Post");
+// import all models
+const Post = require('./Post');
+const User = require('./User');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -8,7 +10,7 @@ User.hasMany(Post, {
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
+    foreignKey: 'user_id'
 });
 
 User.belongsToMany(Post, {
