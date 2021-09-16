98a86e11cb57f0baee529939b986693a657eb596
Joelle DesChatelets <jdeschat@gmail.com>
2021-09-16 10:24:12 -0400

    added authentication

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+
+const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 
-<<<<<<< HEAD:routes/index.js
-=======
 router.use('/', homeRoutes);
->>>>>>> feature/homepage-view:controllers/index.js
 router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.status(404).end();
-});
 
 module.exports = router;
